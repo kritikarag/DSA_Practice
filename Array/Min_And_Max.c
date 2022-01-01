9b09482d82e23d783028f1dc5fe4f04b760b84df
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-01-01 13:25:41 +0530

    Update Min_And_Max.c

--- Array/Min_And_Max.c
+++ Array/Min_And_Max.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 void minmaxLinear(int arr[], int n,int min, int max){ 
     for(int i=1;i<n;i++){
         if(arr[i]>max)max=arr[i];
         if(arr[i]<min)min=arr[i];
     }
-    printf("Minimum element is %d and Maximum element is %d ",min,max);
+    cout<<"Minimum element is "<<min<<" and Maximum element is "<<max;
 }
 
 int main(){
     int min,max,n;
-    printf("Enter the size of array: ");
-    scanf("%d",&n);
+    cout<<"Enter the size of array: ";
+    cin>>n;
     int arr[n];
-    printf("Enter the elements of array: ");
+    cout<<"Enter the elements of array: ";
     for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+        cin>>arr[i];
     }
     minmaxLinear(arr,n,arr[0],arr[0]);
     return 0;
