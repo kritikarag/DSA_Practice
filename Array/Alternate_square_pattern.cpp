f2b5831b3ed994bcfba5375db711ceb811df0c8c
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-01-13 20:22:20 +0530

    Alternate square pattern
    This code prints patterns like:
    1 2 3 4 5
    10 9 8 7 6
    11 12 13 14 15
    20 19 18 17 16
    and so on...

---
+++ Array/Alternate_square_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==1){
+            for(int j = 5*(i-1) +1;j<=5*i;j++)cout<<j<<" ";
+        }
+        else{
+            for(int j=5*i;j>5*(i-1);j--)cout<<j<<" ";
+        }
+        
+        cout<<endl;
+    }
+	// your code goes here
+	return 0;
+}
