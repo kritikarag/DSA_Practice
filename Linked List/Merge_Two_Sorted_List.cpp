1e1c7dd7d11e52b2f960088047afc46fc209c59c
Kritika Rag <72866402+kritikarag@users.noreply.github.com>
2022-04-26 17:05:45 +0530

    Merge Two Sorted Lists

--- Linked List/Merge_Two_Sorted_List.cpp
+++ Linked List/Merge_Two_Sorted_List.cpp
@@ -1,7 +1,15 @@
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        //If list1 is empty then return list2
         if(list1==NULL)return list2;
+        
+        //If list1 is empty then return list2
         if(list2==NULL)return list1;
+        
+        //Create a head pointer for new list
         ListNode*head=list1;
+        
+        //Assign the head pointer the node with the smallest value
         if(list1->val > list2->val){
             head=list2;
             list2=list2->next;
@@ -9,6 +17,8 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         else{
             list1=list1->next;
         }
+        
+        //Create a temp/curr node 
         ListNode* curr=head;
         
         while(list1 && list2){
